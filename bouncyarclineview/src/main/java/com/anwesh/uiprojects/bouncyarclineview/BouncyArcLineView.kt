9cb46ac41c0bcdd2ea9af959f2094b7b6c5b5c1c
Anwesh43 <anweshthecool0@gmail.com>
2019-11-14 10:24:01 +0530

    adding constants and libraries

--- bouncyarclineview/src/main/java/com/anwesh/uiprojects/bouncyarclineview/BouncyArcLineView.kt
+++ bouncyarclineview/src/main/java/com/anwesh/uiprojects/bouncyarclineview/BouncyArcLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bouncyarclineview
 /**
  * Created by anweshmishra on 14/11/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val parts : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#1A237E")
+val backColor : Int = Color.parseColor("#BDBDBD")
